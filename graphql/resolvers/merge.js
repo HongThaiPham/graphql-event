f77f702f5e6d2b16b8a9595c29af87c2186848ae
Pham Hong Thai <leopham@Phams-Mac-mini.local>
2019-04-05 15:38:49 +0700

    #23 finish

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -28,6 +28,11 @@ const getUser = async userId => {
 const getEvents = async eventIds => {
   try {
     const events = await Event.find({ _id: { $in: eventIds } });
+    events.sort((a, b) => {
+      return (
+        eventIds.indexOf(a._id.toString()) - eventIds.indexOf(b._id.toString())
+      );
+    });
     return events.map(event => {
       return tranformEvent(event);
     });
